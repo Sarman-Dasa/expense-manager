1cf141aec883343a7fcd402ad349cd59a600e8cb
Sarman-Dasa <sarmand@zignuts.com>
2023-02-28 20:39:46 +0530

    Route for Authcontroller and AccountController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AccountUserController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//-----------------------------------------//Login Registration//-----------------------------------------//
+Route::controller(AuthController::class)->group(function(){
+    Route::post('registration','register')->name('user.register');
+    Route::get('verifyEmail/{token}','accountVerify')->name('user.verify');
+    Route::post('login','login')->name('user.login');
+});
+//-----------------------------------------//Account//-----------------------------------------//
+Route::controller(AccountController::class)->group(function(){
+    Route::post('create','addAccount')->name('accound.add');
+    Route::patch('update/{id}','updateAccount')->name('account.update');
+    Route::delete('delete/{id}','deleteAccount')->name('account.delete');
+    Route::get('index','index')->name('account.index');
+    Route::get('show/{id}','show')->name('account.show');
+});
+
+
+
+
+// Route::apiResource('my',AccountController::class);
