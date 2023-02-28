df4c211b22e6d1203d8aa90052250da504be525d
Sarman-Dasa <sarmand@zignuts.com>
2023-02-28 20:33:15 +0530

    modell for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
+        'phone',
+        'is_onborded',
         'password',
+        'email_verify_token',
     ];
 
     /**
