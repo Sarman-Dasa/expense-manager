2bdbec377f787a67494d9c368c49d5fb098c84d5
Sarman-Dasa <sarmand@zignuts.com>
2023-02-28 20:35:49 +0530

    factory for user data add

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -17,11 +18,17 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $fname = fake()->firstName();
+        $lname = fake()->lastName();
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name'    => $fname,
+            'last_name'     => $lname,
+            'email'         => fake()->unique()->safeEmail(),
+            'phone'         => fake()->regexify('[6-9]{2}[0-9]{8}'),
+            'role'          => 'Admin',
+            'is_onborded'   => true,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
