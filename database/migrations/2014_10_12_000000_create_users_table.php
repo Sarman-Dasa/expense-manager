f773ceaf74c4cfc9c384e8ad706c18a7f1478b4d
Sarman-Dasa <sarmand@zignuts.com>
2023-02-28 20:36:24 +0530

    migrate for user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('role')->default('user');
+            $table->boolean('is_onborded')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
