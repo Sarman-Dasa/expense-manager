dec627cc91f0b0b5ea5c36a6d853f0a5223cb2ea
Sarman-Dasa <sarmand@zignuts.com>
2023-03-03 18:43:59 +0530

    add expire token validation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,16 +21,16 @@ use Illuminate\Support\Str;
 class AuthController extends Controller
 {
     use ResponseTraits;
-    public function register(Request $request)
-    {
+
+    public function register(Request $request){
         $validation = validator($request->all(),[
-            'first_name'        =>  'required|alpha|max:30',
-            'last_name'         =>  'required|alpha|max:30',
+            'first_name'        =>  'required|string|alpha|max:30',
+            'last_name'         =>  'required|string|alpha|max:30',
             'email'             =>  'required|email|unique:users,email',
             'phone'             =>  'required|digits:10|unique:users,phone',
             'password'          =>  'required|min:8|confirmed',
-            'account_name'      =>  'required|max:100',
-            'account_number'    =>  'required|max:12 ,min:10|unique:accounts,account_number',
+            'account_name'      =>  'required|string|max:100',
+            'account_number'    =>  'required|min_digits:10|max_digits:12|unique:accounts,account_number',
         ]);
 
         if($validation->fails())
@@ -55,8 +55,7 @@ class AuthController extends Controller
         return $this->sendSuccessResponse(true,"Your accound has been create Successfully.");
     }
 
-    public function accountVerify($token)
-    {
+    public function accountVerify($token){
         $user = User::where('email_verify_token' ,'=' ,$token)->first();
         if($user){
             $user->update([
@@ -71,8 +70,7 @@ class AuthController extends Controller
         }
     }
 
-    public function login(Request $request)
-    {
+    public function login(Request $request){
         $validation = validator($request->all() ,[
             'email'     =>  ['required' ,'email' ,'exists:users,email'],
             'password'  =>  ['required'],
@@ -97,8 +95,7 @@ class AuthController extends Controller
         }
     }
 
-    public function sendMailForForgotPassword(Request $request)
-    {
+    public function sendMailForForgotPassword(Request $request){
         $validation = validator($request->all(),[
             'email'     => ['required','email']
         ]);
@@ -115,9 +112,10 @@ class AuthController extends Controller
             $data1 = PasswordReset::updateOrCreate(
             ['email'=> $request->email],
             [
-                'email'=> $request->email,
-                'token'=>$token,
-                'created_at'=>now()
+                'email'         => $request->email,
+                'token'         => $token,
+                'created_at'    => now(),
+                'expired_at'    => now()->addDays(2),
             ]);
             $user['token'] = $token;
            $user->notify(new resetPasswordMail($user));
@@ -129,8 +127,7 @@ class AuthController extends Controller
         }
     }
 
-    public function resetPassword(Request $request)
-    {
+    public function resetPassword(Request $request){
         $validation = validator($request->all(),[
             'email'         =>  ['required' ,'email' ,'exists:password_resets,email'],
             'password'      =>  ['required' ,'min:8' ,'confirmed'],
@@ -141,9 +138,12 @@ class AuthController extends Controller
         {
             return $this->sendErrorResponse($validation);
         }
-
-        $check = PasswordReset::where('email',$request->email)->where('token',$request->token)->first();
-        if($check)
+        $date = date('Y-m-d');
+        //dd(now());
+        $user = PasswordReset::where('email',$request->email)->where('token',$request->token)->first();
+        $dateCheck = $user->expired_at >=now();
+        //dd($dateCheck);
+        if($dateCheck)
         {
             $user = User::where('email',$request->email)->first();
            // dd($user);
@@ -153,75 +153,7 @@ class AuthController extends Controller
             return $this->sendSuccessResponse(true,'password reset successfully.');
         }
         else{
-            return $this->sendFailureResponse("This password reset token is invalid");
-        }
-    }
-
-    public function changePassword(Request $request){
-        
-        $validation = validator($request->all(),[
-            'current_password'  =>  ['required' ,'current_password'],
-            'password'          =>  ['required' ,'min:8' ,'confirmed'],
-        ]);
-
-        if($validation->fails())
-        {
-            return $this->sendErrorResponse($validation);
-        }
-
-        //$id = Auth::user()->id;
-        //$user = User::find($id);
-        $user = auth()->user();
-        $user->update([
-            'password'  => Hash::make($request->password),
-        ]);
-        return $this->sendSuccessResponse(true,'Password change Successfully');
-    }
-
-    public function userProfile($id)
-    {
-        $userProfile   =   User::with('accounts')->find($id);
-       if($userProfile){
-            return $this->sendSuccessResponse(true,"User Profile Data",$userProfile); 
-       }
-       else{
-            return $this->sendFailureResponse("User Not Found!!!");
-       }
-    }
-
-    public function listOfTransactions($id)
-    {
-        try{
-            $listOfTransactions = User::with('transactions')->findOrFail($id);
-            return $this->sendSuccessResponse(true,"Account Transaction(s) get Successfully",$listOfTransactions);
-        }catch(Exception $ex){
-            return $this->sendExecptionMessage($ex);
-        }
-    }
-
-    public function listOfAccount($id)
-    {
-        $listOfAccount  = User::with('accounts')->find($id);
-        if($listOfAccount)
-        {
-            return $this->sendSuccessResponse(true,"User Account Get Successfully",$listOfAccount);
-        }
-        else
-        {
-            return $this->sendFailureResponse("User Not Found!!!");
-        }
-    }
-
-    public function listOfAccountUser($id)
-    {
-        $listOfAccountUser  = User::with('accountUsers')->find($id);
-        if($listOfAccountUser)
-        {
-            return $this->sendSuccessResponse(true,"User Account Get Successfully",$listOfAccountUser);
-        }
-        else
-        {
-            return $this->sendFailureResponse("User Not Found!!!");
+            return $this->sendFailureResponse("This password reset token expired!!!");
         }
     }
 }
