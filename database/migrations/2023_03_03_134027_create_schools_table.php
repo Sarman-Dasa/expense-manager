3f007c1d567474a4b22e8f896ae944a6406ba07a
Sarman-Dasa <sarmand@zignuts.com>
2023-03-03 20:16:59 +0530

    add school table

---
+++ database/migrations/2023_03_03_134027_create_schools_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->id();
+            $table->string('school_name',200);
+            $table->string('email',40)->unique();
+            $table->bigInteger('phone')->unique();
+            $table->string('city');
+            $table->text('address');
+            $table->string('website_link',150)->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+};
