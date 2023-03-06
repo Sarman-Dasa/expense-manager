545c03619a92677e7d6f44b5b75ed7dcbc5956df
Sarman-Dasa <sarmand@zignuts.com>
2023-03-06 19:52:25 +0530

    add school model

--- app/Models/School.php
+++ app/Models/School.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'school_name',
+        'email',
+        'phone',
+        'city',
+        'address',
+        'website_link',
+        'user_id',
+    ];
+
+    //school User relation 
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+    //school teacher relation 
+    public function teachers()
+    {
+        return $this->belongsToMany(Teacher::class,'school_teachers');
+    }
 }
