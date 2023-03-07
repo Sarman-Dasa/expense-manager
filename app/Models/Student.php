e1984c9940001d12d1da748849bd9975fb5bf942
Sarman-Dasa <sarmand@zignuts.com>
2023-03-07 19:36:04 +0530

    add relation : studetn teacher ,Student-user ,Student and school relation

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+   
+    protected $fillable =[
+        'city',
+        'birth_date',
+        'join_date',
+        'school_id',
+        'user_id',
+        'standard',
+        'created_by',
+        'updated_by',
+    ];
+
+    //studetn teacher relation
+    public function teachers()
+    {
+        return $this->belongsToMany(Teacher::class,'teacher_students');
+    }
+
+    //Student Profile
+     public function studentProfile()
+     {
+         return $this->belongsTo(User::class,'user_id');
+     }
+
+
+     //Student and school relation 
+     public function school(){
+        return $this->belongsTo(School::class,'school_id','id');
+     }
 }
