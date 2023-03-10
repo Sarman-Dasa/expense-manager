90377c2c6af7341ead8212f5aa9d148f274de402
Sarman-Dasa <sarmand@zignuts.com>
2023-03-10 17:27:16 +0530

    add traits for  filter list data

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -10,20 +10,51 @@ class EmployeeController extends Controller
 {
     use ResponseTraits;
 
-    public function list()
+    public function list(Request $request)
     {
-        $employees = Employee::paginate(10);
-        //return $this->sendSuccessResponse(true,'Employee(s) Data',$employees);
-        return view('employee',compact('employees'));
+        $query = Employee::query();
+        $searching_Fields = ['name','email','mobile_number','department_name'];
+        return $this->sendFilterListData($query, $searching_Fields);
+    /*    
+        if($request->search){
+            $query->where('name','like',$request->search.'%')
+            ->orWhere('email','like',$request->search)
+            ->orWhere('mobile_number','like',$request->search)
+            ->orWhere('mobile_number','like',$request->search)
+            ->orWhere('hiredate','like',$request->search)
+            ->orWhere('city','like',$request->search)
+            ->orWhere('gender','like',$request->search)
+            ->orWhere('salary','like',$request->search);
+        }
+       if($request->sort)
+        {
+            $sortOrder = $request->sortOrder == 'desc' ? 'desc': 'asc';
+            $query->orderBy($request->sort,$sortOrder);
+        }
+
+        if($request->salary)
+        {
+            $query->where('salary','<=',$request->salary);
+        }
+
+        $pagination = $request->perPageData ?? 10;
+        $employees = $query->paginate($pagination);
+        
+
+        return $this->sendSuccessResponse(true,'Employee(s) Data',$employees);*/
     }
 
-    public function get($query, $value)
+
+    // public function sort($query, $value)
+    // {
+    //     $employees = Employee::orderBy($query,$value)->paginate(10);
+    //     return view('employee',compact('employees'));
+    // }
+     
+    public function webList()
     {
-        if($query=='search')
-            $employees = Employee::where('name','like',$value .'%')->paginate(10);
-        else if($query=='sortBy')
-            $employees = Employee::orderBy($value,'DESC')->paginate(10);
-        return $this->sendSuccessResponse(true,'Employee(s) Data',$employees);
+        $employees = Employee::all();
+        return view('EmployeeList',compact('employees'));
     }
 
 }
